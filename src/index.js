92fd009db81c5b31be3d156c2c50b0e905b2c661
amapy20 <engineer.ama.2021@gmail.com>
2021-04-28 01:45:12 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import "@laylazi/bootstrap-rtl/dist/css/bootstrap-rtl.min.css";
+import "./css/style.css";
+import 'bootstrap';
+import "jquery/dist/jquery";
+import "popper.js/dist/popper";
+import "bootstrap/dist/js/bootstrap";
+import "@fortawesome/fontawesome-free/js/all";
+
+$(document).ready(function(){
+    $("[data-toggle='tooltip']").tooltip();
+});
